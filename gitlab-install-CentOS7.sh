2bb940d1a57edb7e2e0fe4c29a32f61c8ba9f48f
Majid Arabgol <marabgol@Majids-MacBook-Pro-2.local>
2018-09-30 10:49:27 -0400

    script to install gitlab

---
+++ gitlab-install-CentOS7.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+sudo yum install -y curl policycoreutils-python openssh-server
+
+sudo systemctl enable sshd
+sudo systemctl start sshd
+sudo firewall-cmd --permanent --add-service=http
+sudo systemctl reload firewalld
+
+sudo yum install postfix -y
+sudo systemctl enable postfix
+sudo systemctl start postfix
+
+
+curl https://packages.gitlab.com/install/repositories/gitlab/gitlab-ee/script.rpm.sh | sudo bash
+
+IP=`ifconfig |grep inet |awk '{print $2}' |xargs -I % echo % | egrep -e "^[1-9][0-9][0-9]" |grep  -v ".1$"`
+
+sudo EXTERNAL_URL="http://$IP" yum install -y gitlab-ee
