e5dc07e3f62444152ddd43e08e988d0541536bfe
Majid Arabgol <13124046+marabgol@users.noreply.github.com>
2018-10-12 18:31:23 -0400

    we will use port-forwarding so localhost should do

--- gitlab-install-CentOS7.sh
+++ gitlab-install-CentOS7.sh
@@ -16,6 +16,6 @@ sudo systemctl start postfix
 
 curl https://packages.gitlab.com/install/repositories/gitlab/gitlab-ee/script.rpm.sh | sudo bash
 
-IP=`ifconfig |grep inet |awk '{print $2}' |xargs -I % echo % | egrep -e "^[1-9][0-9][0-9]" |grep  -v ".1$"`
+#IP=`ifconfig |grep inet |awk '{print $2}' |xargs -I % echo % | egrep -e "^[1-9][0-9][0-9]" |grep  -v ".1$"`
 
-sudo EXTERNAL_URL="http://$IP" yum install -y gitlab-ee
+sudo EXTERNAL_URL="http://localhost" yum install -y gitlab-ee
